eeb55c67117caef76c9e8043ad57a20453651ab9
caitlin-s-codes <caitlin.stevens0707@gmail.com>
2024-09-20 13:23:24 +0100

    Added basic search engine behaviour.

---
+++ src/index.js
@@ -0,0 +1,10 @@
+function searchCity(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  console.log(searchInput.value);
+  let cityElement = document.querySelector("#current-city");
+  cityElement.innerHTML = searchInput.value;
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", searchCity);
